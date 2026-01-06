c38b4098deb9f389400dc81c131db7cbf1c50563
meng hor <piseth125125@gmail.com>
2026-01-06 16:21:09 +0700

    add file controller

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.frontend')
+
+@section('content')
+<div class="text-center mt-10">
+    <h1 class="text-4xl font-bold">Welcome to School Management System</h1>
+    <p class="mt-4 text-gray-700">Manage your courses, students, and teachers efficiently!</p>
+</div>
+@endsection
